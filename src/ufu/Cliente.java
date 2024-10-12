8a4e3bf93fa7505c120232405b3c8bebfb01cac9
Cassio237 <114037787+Cassio237@users.noreply.github.com>
2024-10-12 00:47:36 -0300

    Inicio
    Criando e testando repositorio

---
+++ src/ufu/Cliente.java
@@ -0,0 +1,35 @@
+package ufu;
+
+public abstract class Cliente {
+	private String nome, endereco, telefone;
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
---
+++ src/ufu/Main.java
@@ -0,0 +1,14 @@
+package ufu;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+
+	}
+
+}
